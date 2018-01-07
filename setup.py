412b0fdffc75b539cb72db4a8a2e67be2cca1983
Paul Aan <weburnittv@gmail.com>
2018-01-07 13:13:41 +0700

    Update Author info
    add name and email through setup file

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     packages=['historio', 'historio.client', 'historio.core', 'historio.definition'],
     include_package_data=True,
     version='0.2',
+    author='Paul Aan',
+    author_email='weburnittv@gmail.com',
     install_requires=[
         'mongoengine',
         'grpcio',
